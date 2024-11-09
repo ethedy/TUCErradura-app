a8d5f077649272306ad017da1c003c1d5b2cbefa
BrianStumpp <brian000stumpp@gmail.com>
2024-11-09 09:05:59 -0300

    background1

--- scratch/flutter_application_1/lib/Screens/Sign_In_Page.dart
+++ scratch/flutter_application_1/lib/Screens/Sign_In_Page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'LOGIN',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SvgPicture.asset(
+            "assets/icons/Background.svg",
+            height: size.height * 0.35,
+          )
+        ],
+      ),
+    );
   }
 }
