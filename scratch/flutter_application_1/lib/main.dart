16d78c7cb45370845e03456bd071a93efc971e1b
Lauti-01 <lautarovicens@gmail.com>
2024-12-18 19:02:47 -0300

    Cambios Login

--- scratch/flutter_application_1/lib/main.dart
+++ scratch/flutter_application_1/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/screens.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,78 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Control de LED ESP8266',
+      debugShowCheckedModeBanner: false,
+      title: 'TUSErradura',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final String esp8266Ip =
-      'http://192.168.100.79'; // Reemplazar a IP del ESP8266
-  List<String> log = [];
-
-  Future<void> _sendRequest(String action) async {
-    final url = '$esp8266Ip/door/$action';
-    final response = await http.get(Uri.parse(url));
-
-    if (response.statusCode == 200) {
-      setState(() {
-        log.add('Acción: $action, Respuesta: ${response.body}');
-      });
-    } else {
-      setState(() {
-        log.add('Error al enviar la solicitud.');
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('PUERTA - ESP8266'),
-        shadowColor: Colors.grey,
-        scrolledUnderElevation: 20.0,
-      ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () => _sendRequest('open'),
-                child: const Text('ABRIR'),
-              ),
-              const SizedBox(width: 50),
-              ElevatedButton(
-                onPressed: () => _sendRequest('closed'),
-                child: const Text('CERRAR'),
-              ),
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: log.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(log[index]),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+      home: Screens(),
     );
   }
 }
--- scratch/flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
+++ scratch/flutter_application_1/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import google_sign_in_ios
+import network_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
 }
